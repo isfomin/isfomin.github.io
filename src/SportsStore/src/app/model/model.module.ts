c5c834596eb2a6fde265dd4c0a247d2ef3829471
SFomin <sergei.fomin@list.ru>
2019-06-14 21:08:49 +0500

    Добавил новый источник данных

--- src/SportsStore/src/app/model/model.module.ts
+++ src/SportsStore/src/app/model/model.module.ts
@@ -5,11 +5,12 @@ import { Cart } from "./cart.model";
 import { Order } from "./order.model";
 import { OrderRepository } from "./order.repository";
 import { RestDataSource } from "./rest.datasource";
+import { Rest2DataSource } from "./rest2.datasource";
 import { HttpModule } from "@angular/http";
 
 @NgModule({
   imports: [HttpModule],
   providers: [ProductRepository, Cart, Order, OrderRepository,
-    { provide: StaticDataSource, useClass: StaticDataSource }]
+    { provide: StaticDataSource, useClass: Rest2DataSource }]
 })
 export class ModelModule { }
---
+++ src/SportsStore/src/app/model/rest2.datasource.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { Http, Request, RequestMethod } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import { Product } from "./product.model";
+import { Cart } from "./cart.model";
+import { Order } from "./order.model";
+import "rxjs/add/operator/map";
+
+@Injectable()
+export class Rest2DataSource {
+  baseUrl:string;
+
+  constructor(private http:Http) {
+    this.baseUrl = "https://my-json-server.typicode.com/isfomin/json-data/";
+  }
+
+  getProducts():Observable<Product[]> {
+    return this.sendRequest(RequestMethod.Get, "products");
+  }
+
+  saveOrder(order:Order):Observable<Order> {
+    return this.sendRequest(RequestMethod.Post, "orders", order);
+  }
+
+  private sendRequest(verb:RequestMethod, url:string, body?:Product | Order):Observable<any> {
+    return this.http.request(new Request({
+      method: verb,
+      url: this.baseUrl + url,
+      body: body
+    })).map(response => response.json());
+  }
+}
